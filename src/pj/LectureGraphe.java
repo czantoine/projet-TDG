c2a12c4446468c7422e726c092bd40c3311538a7
Julie Neige <37273526+Juu-Snw@users.noreply.github.com>
2020-04-09 11:14:54 +0200

    creation fichier
    lecture fichier

---
+++ src/pj/LectureGraphe.java
@@ -0,0 +1,18 @@
+package pj;
+
+import java.util.Scanner;
+
+public class LectureGraphe {
+	
+	private Scanner sc;
+
+	public LectureGraphe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
