0546190fe2180d0d104b3f4f6bc0336c98d96570
czantoine <ac11f@free.fr>
2020-04-13 09:47:47 +0200

    entre et sortie

--- python/projet.py
+++ python/projet.py
@@ -101,6 +101,71 @@ def detcir(L2):
                         print("Il n'y a pas de circuit")
                         return False
             
+def calcul_rang(Ta,Lf=[]):
+    Lpe = []
+    v = 1
+    Li = Ta[0]
+    Tar =  [Ta[0]]
+    if len(Ta) == 1:
+        return Lf
+    else :
+        for j in range (1,len(Ta[0])):
+            for i in range (1,len(Ta)):
+                if Ta[i][j] == 'F':
+                    Val = False
+                else :
+                    Val = True
+                    break
+                if Val == False :
+                    Lpe.append(Ta[v][0])
+                else :
+                    Tar = Tar+[Ta[j]]
+                v+=1
+
+def pointdentree(L2):
+    LC = list(L2)
+    Listefinale = []
+    n = len(L2)
+    for k in range (n):
+        for j in range (n):
+            if L2[k][0] == L2[j][1]:
+                LC[k] = 0
+    for p in range (0,n):
+        if LC[p]!=0:
+            Listefinale.append(LC[p][0])
+    Listefinale = list(set(Listefinale))
+    
+    a= len (Listefinale)
+    if a != 1 :
+        print("Il y a plusieurs points d'entrées",Listefinale[0])
+        return False
+    if a == 1 :
+        print("Il n'y a qu'un seul point d'entrée", Listefinale[0])
+        return Listefinale[0]
+    
+def pointdesortie(L2):
+    LC = list(L2)
+    Listefinale = []
+    n = len(L2)
+    for k in range (n):
+        for j in range (n):
+            if L2[k][1] == L2[j][0]:
+                LC[k] = 0
+    for p in range (0,n):
+        if LC[p]!=0:
+            Listefinale.append(LC[p][1])
+    Listefinale = list(set(Listefinale))
+    
+    a= len (Listefinale)
+    if a != 1 :
+        print("Il y a plusieurs points de sorties",Listefinale[0])
+        return False
+    if a == 1 :
+        print("Il n'y a qu'un seul point de sorite", Listefinale[0])
+        return Listefinale[0]
+    
+    
+        
     
  
 def projet():
@@ -132,7 +197,14 @@ def projet():
             print("")
             dateplutot = plutot(Listearc,rangs)
             print("")
-        
+            print("Calendrier au plus tot :")
+            print("")
+            print(dateplutot)
+            dateplutot = plutard(Listearc,rangs,dateplutot)
+            print("")
+            print("Calendrier au plus tard")
+            print("")
+            print(dateplutard)
             
         
    
